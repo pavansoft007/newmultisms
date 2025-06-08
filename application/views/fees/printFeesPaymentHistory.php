1f61c54c187292f73b62bb2367c03651f6400d8f
Venki1992kv <venki1992kv@gmail.com>
2025-06-08 22:29:08 +0530

    INVOICE SETTINGS

--- application/views/fees/printFeesPaymentHistory.php
+++ application/views/fees/printFeesPaymentHistory.php
@@ -9,11 +9,15 @@ if ($extINTL == true) {
 	<div class="col-lg-5 pull-right">
 		<ul class="amounts">
 			<li><strong><?=translate('sub_total')?> :</strong> <?=$currency_symbol . number_format($total_paid + $total_discount, 2, '.', ''); ?></li>
+			<?php if ($total_discount >= 1): ?>
 			<li><strong><?=translate('discount')?> :</strong> <?=$currency_symbol . number_format($total_discount, 2, '.', ''); ?></li>
+			<?php endif; ?>
 			<li><strong><?=translate('paid')?> :</strong> <?=$currency_symbol . number_format($total_paid, 2, '.', ''); ?></li>
+			<?php if ($total_fine >= 1): ?>
 			<li><strong><?=translate('fine')?> :</strong> <?=$currency_symbol . number_format($total_fine, 2, '.', ''); ?></li>
+			<?php endif; ?>
 			<li>
-				<strong><?=translate('total_paid')?> (<?=translate('with_fine')?>) : </strong> 
+				<strong><?=translate('total_paid')?><?php if ($total_fine >= 1): ?> (<?=translate('with_fine')?>)<?php endif; ?> : </strong> 
 				<?php
 				$numberSPELL = "";
 				$grand_paid = number_format($total_paid + $total_fine, 2, '.', '');
